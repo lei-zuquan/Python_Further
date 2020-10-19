548dd27fb41c211de7c997670aa2cbd269f57c47
leizuquan1990 <843291011@qq.com>
2020-10-19 22:58:59 +0800

    # 首次提交Python 研究

---
+++ c01_basic.py
@@ -0,0 +1,56 @@
+# 打印helloWorld
+print("hello world")
+# print("1")
+# print("2")
+# print("3")
+
+# ======= 3.1.1 使用print() 函数输出
+# a = 10
+# b = 6
+# print("数字", 6)
+# print("表达式", a*b)
+# print("字符串:", "成功的秘诀——坚持最后一分钟")
+#
+# fp = open("mot.txt", 'a+')
+# print("成功的唯一秘诀——坚持最后一分钟", file = fp) # 输出到文件中
+# fp.close() # 关闭文件
+
+
+# ======= 3.1.2 使用input() 函数输入
+# variable = input("提示文字")
+# tip = input("请输入文字：")
+# print(tip)
+# print(type(tip)) # 虽然我们输入的是数字，但类型是字符串
+# print(type(int(tip))) # 将字符串强制转成整型
+
+# 实例：根据身高、体重计算BMI指数(改进版)
+# height = float(input("请输入您的身高（单位为米）：")) # 保存身高的变量，单位为米
+# print("您的身高：", height)
+# weight = float(input("请输入您的体重（单位为千克）：")) # 保存体重的变量，单位为kg
+# print("您的体重：", weight)
+#
+# bmi = weight / (height*height) # 计算BMI指数
+# print("您的BMI指数：", bmi)
+# if bmi < 18.5:
+#     print("您的体重过轻")
+# if bmi >= 18.5 and bmi < 24.9:
+#     print("正常范围，注意保持")
+# if bmi >= 24.9 and bmi < 29.9:
+#     print("您的体重过重")
+# if bmi >= 29.9:
+#     print("肥胖")
+
+
+# ======= 3.2 注释规则
+# 概念：注释是指在程序代码中添加的标注性的文字。
+
+# 怎么注释？
+# 1.单行注释，# 注释内容
+# 2.多行注释，
+#       在Python 中将包含一对三引号（'''......'''）或者("""......""")之间，并且不属于任何语句的内容认为是多行注释。
+
+
+
+
+
+
